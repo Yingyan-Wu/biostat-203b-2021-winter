8b8745f92b1e5e27e343718d071487232d226421
ywu <wuyingyan1120@gmail.com>
2021-03-05 00:56:17 -0800

    Output tables and plots

--- hw3/mimiciv_shiny/app.R
+++ hw3/mimiciv_shiny/app.R
@@ -3,7 +3,8 @@ library(shiny)
 library(tidyverse)
 
 # Source helpers ----
-source("helpers.R")
+# didn't use!
+# source("helpers.R")
 
 #---- Load data ----
 icu_cohort <- readRDS("icu_cohort.rds")
@@ -11,18 +12,18 @@ icu_cohort <- readRDS("icu_cohort.rds")
 #---- Set up variables ----
 # colnames(icu_cohort)
 Admissionsvar <- c("admission_type", "flag_die_30", "insurance")
-Demovar <- c("age_at_admission", "gender", "ethnicity", "language", 
+demovar <- c("age_at_admission", "gender", "ethnicity", "language", 
              "insurance", "marital_status")
-Labvar <- c("bicarbonate", "calcium", "chloride", "creatinine", "glucose", 
+labvar <- c("bicarbonate", "calcium", "chloride", "creatinine", "glucose", 
             "magnesium", "potassium", "sodium", "hematocrit", "wbc", "lactate")
-Vitalvar <- c("heart_rate", 
+vitalvar <- c("heart_rate", 
                  "non_invasive_blood_pressure_systolic",
                  "non_invasive_blood_pressure_mean",
                  "respiratory_rate",
                  "temperature_fahrenheit",
-                 "wrterial_blood_pressure_systolic",
                  "arterial_blood_pressure_mean",
                  "flag_die30")
+catvar <- c("gender", "ethnicity", "language", "insurance", "marital_status")
 
 #---- User interface ----
 ui <- fluidPage(
@@ -36,33 +37,33 @@ ui <- fluidPage(
              sidebarLayout(
                sidebarPanel(
                  # variables input
-                 selectInput(inputId = "Demovar", 
+                 selectInput(inputId = "demovar", 
                              label = "Demographic information:",
-                             choices = Demovar,
-                             selected = "age_at_admission"),
-                 # stratified by admission types
-                 checkboxInput(inputId = "stratified", 
-                               label = "Stratified",
-                               value = FALSE),
-                 selectInput(inputId = "Admissionsvar",
-                             label = "Stratified by:",
-                             choices = Admissionsvar,
-                             # selected = NULL,
-                             selected = "flag_die_30")
+                             choices = demovar,
+                             selected = "age_at_admission")
+                 # # stratified by admission types
+                 # checkboxInput(inputId = "stratified", 
+                 #               label = "Stratified",
+                 #               value = FALSE),
+                 # selectInput(inputId = "Admissionsvar",
+                 #             label = "Stratified by:",
+                 #             choices = Admissionsvar,
+                 #             # selected = NULL,
+                 #             selected = "flag_die_30")
                ),
                mainPanel(
                  # summary statistics table
-                 tableOutput("tableDemo"),
+                 tableOutput("demotable"),
                  helpText("Distribution:"),
-                 # sliderInput("binsDemo",
-                 #             "Number of bins:",
-                 #             min = 1,
-                 #             max = 50,
-                 #             value = 30),
-                 plotOutput("plotDemo")
-                 # helpText("Boxplot:"),
-                 # plotOutput("boxplotDemo"),
-                 )
+                 sliderInput("binsDemo",
+                             "Number of bins:",
+                             min = 1,
+                             max = 50,
+                             value = 30),
+                 plotOutput("demoplot"),
+                 helpText("Boxplot:"),
+                 plotOutput("demoboxplot")
+               )
              ),
              
     ),
@@ -72,32 +73,32 @@ ui <- fluidPage(
              sidebarLayout(
                sidebarPanel(
                  # variables input
-                 selectInput(inputId = "Labvar ", 
+                 selectInput(inputId = "labvar", 
                              label = "Lab measurement:",
-                             choices = Labvar,
-                             selected = "bicarbonate"),
-                 # stratified by admission types
-                 checkboxInput(inputId = "stratified", 
-                               label = "Stratified",
-                               value = FALSE),
-                 selectInput(inputId = "Admissionsvar",
-                             label = "Stratified by:",
-                             choices = Admissionsvar,
-                             # selected = NULL,
-                             selected = "flag_die_30")
+                             choices = labvar,
+                             selected = "bicarbonate")
+                 # # stratified by admission types
+                 # checkboxInput(inputId = "stratified", 
+                 #               label = "Stratified",
+                 #               value = FALSE),
+                 # selectInput(inputId = "Admissionsvar",
+                 #             label = "Stratified by:",
+                 #             choices = Admissionsvar,
+                 #             # selected = NULL,
+                 #             selected = "flag_die_30")
                ),
-              mainPanel(
+               mainPanel(
                  # summary statistics table
-                 tableOutput("tablelab"),
+                 tableOutput("labtable"),
                  helpText("Distribution:"),
-                 # sliderInput("binslab",
-                 #             "Number of bins:",
-                 #             min = 1,
-                 #             max = 50,
-                 #             value = 30),
-                 plotOutput("plotlab")
-                 # helpText("Boxplot:"),
-                 # plotOutput("boxplotlab"),
+                 sliderInput("binslab",
+                             "Number of bins:",
+                             min = 1,
+                             max = 50,
+                             value = 30),
+                 plotOutput("labplot"),
+                 helpText("Boxplot:"),
+                 plotOutput("labboxplot")
                )
              )
     ),
@@ -106,51 +107,123 @@ ui <- fluidPage(
     tabPanel("Vital status", 
              sidebarLayout(
                sidebarPanel(
-                 selectInput("Vitalvar", "Vitals:",
-                             Vitalvar,
-                             selected = "heart_rate"),
-                 # stratified by admission types
-                 checkboxInput(inputId = "stratified", 
-                               label = "Stratified",
-                               value = FALSE),
-                 selectInput(inputId = "Admissionsvar",
-                             label = "Stratified by:",
-                             choices = Admissionsvar,
-                             # selected = NULL,
-                             selected = "flag_die_30")
+                 selectInput("vitalvar", "Vitals:",
+                             vitalvar,
+                             selected = "heart_rate")
+                 # # stratified by admission types
+                 # checkboxInput(inputId = "stratified", 
+                 #               label = "Stratified",
+                 #               value = FALSE),
+                 # selectInput(inputId = "Admissionsvar",
+                 #             label = "Stratified by:",
+                 #             choices = Admissionsvar,
+                 #             # selected = NULL,
+                 #             selected = "flag_die_30")
                ),
                mainPanel(
                  # summary statistics table
-                 tableOutput("tablevital"),
+                 tableOutput("vitaltable"),
                  helpText("Distribution:"),
-                 # sliderInput("binsvital",
-                 #             "Number of bins:",
-                 #             min = 1,
-                 #             max = 50,
-                 #             value = 30),
-                 plotOutput("plotvital")
-                 # helpText("Boxplot:"),
-                 # plotOutput("boxplotvital"),
+                 sliderInput("binsvital",
+                             "Number of bins:",
+                             min = 1,
+                             max = 50,
+                             value = 30),
+                 plotOutput("vitalplot"),
+                 helpText("Boxplot:"),
+                 plotOutput("vitalboxplot")
                )
              )
     )
+  )
+)
   
 
 
 #---- Server logic ----
 server <- function(input, output) {
-  # dataInput_demo <- reactive(
-  #   Demovar <- input$Demovar
-  # )
-  # 
-  # output$distPlotdemo <- renderPlot({
-  #   
-  # })
-  # output$distTable <- {
-  #   results_bootsamples %>%
-  #     kbl(caption = "Direct and indirect effects of X on Y through M")%>%
-  #     kable_classic(full_width = F, html_font = "Arial")
+  # Demo panel
+  demovarinput <- reactive(
+    demovar <- input$demovar
+  )
+  
+  
+  # if(!is.numeric(demovarinput())){
+  #   output$demotable <- renderTable(icu_cohort %>%
+  #                                     count(!!sym(input$demovar)) %>%
+  #                                     mutate(prop = prop.table(n))
+  #                                   )
+  # } else {
+    output$demotable <- renderTable(
+      icu_cohort %>%
+        summarize(
+          N = length(!!sym(input$demovar)),
+          Mean = mean(!!sym(input$demovar), na.rm = T),
+          Std = sd(!!sym(input$demovar), na.rm = T),
+          Min = min(!!sym(input$demovar), na.rm = T),
+          Median = median(!!sym(input$demovar), na.rm = T),
+          Max = max(!!sym(input$demovar), na.rm = T),
+          `N missing` = sum(is.na(!!sym(input$demovar))
+          ))
+    )
+    
   # }
+ 
+  
+  output$labtable <- renderTable(
+    icu_cohort %>%
+      summarize(
+        N = length(!!sym(input$labvar)), 
+        Mean = mean(!!sym(input$labvar), na.rm = T),
+        Std = sd(!!sym(input$labvar), na.rm = T),
+        Min = min(!!sym(input$labvar), na.rm = T),
+        Median = median(!!sym(input$labvar), na.rm = T),
+        Max = max(!!sym(input$labvar), na.rm = T),
+        `N missing` = sum(is.na(!!sym(input$labvar))
+        ))
+  )
+  
+  output$labplot <- renderPlot(
+    icu_cohort %>%
+      ggplot(., aes_string(x = input$labvar)) +
+      geom_histogram(bins = input$binslab, 
+                     colour = "grey", fill = "light blue") +
+      #Pending label
+      # xlab() +
+      ylab("Density")
+  )
+
+  output$vitaltable <- renderTable(
+    icu_cohort %>%
+      summarize(
+        N = length(!!sym(input$vitalvar)),
+        Mean = mean(!!sym(input$vitalvar), na.rm = T),
+        Std = sd(!!sym(input$vitalvar), na.rm = T),
+        Min = min(!!sym(input$vitalvar), na.rm = T),
+        Median = median(!!sym(input$vitalvar), na.rm = T),
+        Max = max(!!sym(input$vitalvar), na.rm = T),
+        `N missing` = sum(is.na(!!sym(input$vitalvar))
+        ))
+  )
+  
+  output$vitalplot <- renderPlot(
+    icu_cohort %>%
+      ggplot(., aes_string(x = log(input$vitalvar))) +
+      geom_histogram(bins = input$binsvital, 
+                     colour = "grey", fill = "light blue") +
+      #Pending label
+      # xlab() +
+      ylab("Density")
+  )
+  
+ #  output$distPlotdemo <- renderPlot({
+ # 
+ #  })
+ #  output$distTable <- {
+ #    icu_cohort %>%
+ #      kbl(caption = "Direct and indirect effects of X on Y through M")%>%
+ #      kable_classic(full_width = F, html_font = "Arial")
+ #  }
  }
 
 
