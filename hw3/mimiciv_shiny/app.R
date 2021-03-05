b430eec80b5e3409748397c70117496d9032c287
ywu <wuyingyan1120@gmail.com>
2021-03-05 11:29:08 -0800

    log transform, conti/cate variables separated Plots and tables

--- hw3/mimiciv_shiny/app.R
+++ hw3/mimiciv_shiny/app.R
@@ -11,7 +11,7 @@ icu_cohort <- readRDS("icu_cohort.rds")
 
 #---- Set up variables ----
 # colnames(icu_cohort)
-Admissionsvar <- c("admission_type", "flag_die_30", "insurance")
+Admissionsvar <- c("admission_type", "flag_die30", "insurance")
 demovar <- c("age_at_admission", "gender", "ethnicity", "language", 
              "insurance", "marital_status")
 labvar <- c("bicarbonate", "calcium", "chloride", "creatinine", "glucose", 
@@ -21,9 +21,10 @@ vitalvar <- c("heart_rate",
                  "non_invasive_blood_pressure_mean",
                  "respiratory_rate",
                  "temperature_fahrenheit",
-                 "arterial_blood_pressure_mean",
-                 "flag_die30")
-catvar <- c("gender", "ethnicity", "language", "insurance", "marital_status")
+                "arterial_blood_pressure_systolic",
+                 "arterial_blood_pressure_mean", "flag_die30")
+catvar <- c("gender", "ethnicity", "language", "insurance", "marital_status", 
+            "flag_die30")
 
 #---- User interface ----
 ui <- fluidPage(
@@ -42,7 +43,7 @@ ui <- fluidPage(
                              choices = demovar,
                              selected = "age_at_admission")
                  # # stratified by admission types
-                 # checkboxInput(inputId = "stratified", 
+                 # checkboxInput(inputId = "stratified",
                  #               label = "Stratified",
                  #               value = FALSE),
                  # selectInput(inputId = "Admissionsvar",
@@ -53,15 +54,16 @@ ui <- fluidPage(
                ),
                mainPanel(
                  # summary statistics table
+                 helpText("Summary stastistics:"),
                  tableOutput("demotable"),
-                 helpText("Distribution:"),
-                 sliderInput("binsDemo",
-                             "Number of bins:",
-                             min = 1,
-                             max = 50,
-                             value = 30),
+                 helpText("Distribution plot:"),
+                 sliderInput(
+                   "binsdemo",
+                   "# of bins for histogram (Not applicable for bar charts):",
+                   min = 1,
+                   max = 50,
+                   value = 30),
                  plotOutput("demoplot"),
-                 helpText("Boxplot:"),
                  plotOutput("demoboxplot")
                )
              ),
@@ -76,9 +78,14 @@ ui <- fluidPage(
                  selectInput(inputId = "labvar", 
                              label = "Lab measurement:",
                              choices = labvar,
-                             selected = "bicarbonate")
+                             selected = "bicarbonate"),
+                 # Log transform or not
+                 checkboxInput(
+                   "lablog", 
+                   "log scale (only applied to continuous variables)",
+                   value = F)
                  # # stratified by admission types
-                 # checkboxInput(inputId = "stratified", 
+                 # checkboxInput(inputId = "stratified",
                  #               label = "Stratified",
                  #               value = FALSE),
                  # selectInput(inputId = "Admissionsvar",
@@ -86,16 +93,19 @@ ui <- fluidPage(
                  #             choices = Admissionsvar,
                  #             # selected = NULL,
                  #             selected = "flag_die_30")
+                 
                ),
                mainPanel(
                  # summary statistics table
+                 helpText("Summary stastistics:"),
                  tableOutput("labtable"),
                  helpText("Distribution:"),
                  sliderInput("binslab",
-                             "Number of bins:",
+                             "# of bins for histogram:",
                              min = 1,
                              max = 50,
                              value = 30),
+                 helpText("Histogram:"),
                  plotOutput("labplot"),
                  helpText("Boxplot:"),
                  plotOutput("labboxplot")
@@ -107,9 +117,15 @@ ui <- fluidPage(
     tabPanel("Vital status", 
              sidebarLayout(
                sidebarPanel(
+                 # variables input
                  selectInput("vitalvar", "Vitals:",
                              vitalvar,
-                             selected = "heart_rate")
+                             selected = "heart_rate"),
+                 # Log transform or not
+                 checkboxInput(
+                   "vitallog", 
+                   "log scale (only applied to continuous variables)",
+                   value = F)
                  # # stratified by admission types
                  # checkboxInput(inputId = "stratified", 
                  #               label = "Stratified",
@@ -122,13 +138,16 @@ ui <- fluidPage(
                ),
                mainPanel(
                  # summary statistics table
+                 helpText("Summary stastistics:"),
                  tableOutput("vitaltable"),
                  helpText("Distribution:"),
-                 sliderInput("binsvital",
-                             "Number of bins:",
-                             min = 1,
-                             max = 50,
-                             value = 30),
+                 sliderInput(
+                   "binsvital",
+                   "# of bins for histogram(Not applicable for bar charts): ",
+                   min = 1,
+                   max = 50,
+                   value = 30),
+                 helpText("Histogram:"),
                  plotOutput("vitalplot"),
                  helpText("Boxplot:"),
                  plotOutput("vitalboxplot")
@@ -137,94 +156,217 @@ ui <- fluidPage(
     )
   )
 )
-  
+
 
 
 #---- Server logic ----
 server <- function(input, output) {
-  # Demo panel
-  demovarinput <- reactive(
-    demovar <- input$demovar
-  )
-  
-  
-  # if(!is.numeric(demovarinput())){
-  #   output$demotable <- renderTable(icu_cohort %>%
-  #                                     count(!!sym(input$demovar)) %>%
-  #                                     mutate(prop = prop.table(n))
-  #                                   )
-  # } else {
+  #---- Demo panel ----
+    # demovarinput <- reactive(
+    #   demovar <- input$demovar
+    # )
+    
+    #---- **Table ----
     output$demotable <- renderTable(
-      icu_cohort %>%
-        summarize(
-          N = length(!!sym(input$demovar)),
-          Mean = mean(!!sym(input$demovar), na.rm = T),
-          Std = sd(!!sym(input$demovar), na.rm = T),
-          Min = min(!!sym(input$demovar), na.rm = T),
-          Median = median(!!sym(input$demovar), na.rm = T),
-          Max = max(!!sym(input$demovar), na.rm = T),
-          `N missing` = sum(is.na(!!sym(input$demovar))
-          ))
+      if(input$demovar %in% catvar){
+        icu_cohort %>%
+          count(!!sym(input$demovar)) %>%
+          mutate(prop = prop.table(n))
+        
+      } else {
+        icu_cohort %>%
+          summarize(
+            N = length(!!sym(input$demovar)),
+            Mean = mean(!!sym(input$demovar), na.rm = T),
+            Std = sd(!!sym(input$demovar), na.rm = T),
+            Min = min(!!sym(input$demovar), na.rm = T),
+            Median = median(!!sym(input$demovar), na.rm = T),
+            Max = max(!!sym(input$demovar), na.rm = T),
+            `N missing` = sum(is.na(!!sym(input$demovar))
+            ))
+      }
+    )
+    #---- **Histogram/barplot ----
+    output$demoplot <- renderPlot(
+      if(input$demovar %in% catvar){
+        icu_cohort %>%
+          ggplot(., aes_string(x = input$demovar)) + 
+          geom_bar(aes_string(x = input$demovar)) +
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) + 
+          labs(title = paste0("Bar chart of ", input$demovar))
+      } else{
+        icu_cohort %>%
+          ggplot(., aes_string(x = input$demovar)) +
+          geom_histogram(bins = input$binsdemo, 
+                         colour = "black", fill = "grey") +
+          #Pending label
+          # xlab() +
+          ylab("Density") +
+          labs(title = paste0("Histogram of ", input$demovar))
+       }
     )
     
-  # }
- 
+  #---- Lab panel ----
+    #---- **Table ----
+    output$labtable <- renderTable(
+      if (input$lablog){
+        icu_cohort %>%
+          summarize(
+            N = length(log(!!sym(input$labvar))), 
+            Mean = mean(log(!!sym(input$labvar)), na.rm = T),
+            Std = sd(log(!!sym(input$labvar)), na.rm = T),
+            Min = min(log(!!sym(input$labvar)), na.rm = T),
+            Median = median(log(!!sym(input$labvar)), na.rm = T),
+            Max = max(log(!!sym(input$labvar)), na.rm = T),
+            `N missing` = sum(is.na(!!sym(input$labvar))
+            ))
+      } else{
+        icu_cohort %>%
+          summarize(
+            N = length(!!sym(input$labvar)), 
+            Mean = mean(!!sym(input$labvar), na.rm = T),
+            Std = sd(!!sym(input$labvar), na.rm = T),
+            Min = min(!!sym(input$labvar), na.rm = T),
+            Median = median(!!sym(input$labvar), na.rm = T),
+            Max = max(!!sym(input$labvar), na.rm = T),
+            `N missing` = sum(is.na(!!sym(input$labvar))
+            ))
+      }
+      
+    )
+    
+    #---- **Histogram ----
+    output$labplot <- renderPlot(
+      {
+        if (input$lablog){
+          icu_cohort %>%
+            ggplot(., aes(x = log(!!sym(input$labvar)))) +
+            geom_histogram(bins = input$binslab, 
+                           colour = "black", fill = "grey") +
+            #Pending label
+            # xlab() +
+            ylab("Density")
+          
+        } else{
+          icu_cohort %>%
+            ggplot(., aes_string(x = input$labvar)) +
+            geom_histogram(bins = input$binslab, 
+                           colour = "black", fill = "grey") +
+            #Pending label
+            # xlab() +
+            ylab("Density")
+        }
+      }, 
+      height = 400, width = 600
+    ) 
+    #---- **Boxplot ----
+    output$labboxplot <- renderPlot(
+      {
+        if (input$lablog){
+          icu_cohort %>%
+            ggplot(., aes(x = log(!!sym(input$labvar)))) +
+            geom_boxplot(color = "black", fill = "grey") +
+            xlab(input$labvar)
+        } else{
+          icu_cohort %>%
+            ggplot(., aes_string(x = input$labvar)) +
+            geom_boxplot(color = "black", fill = "grey") +
+            xlab(input$labvar)
+        }
+      }, 
+      height = 400, width = 600
+    )
+    
+  #---- Vital status panel ----
+    # vitalloginput <- reactive(
+    #   vitallog <- input$vitallog
+    # )
+    
+    output$vitaltable <- renderTable(
+      if(input$vitalvar %in% catvar){
+        icu_cohort %>%
+          count(!!sym(input$vitalvar)) %>%
+          mutate(prop = prop.table(n))
+        
+      } else {
+        if (input$vitallog){
+          icu_cohort %>%
+            summarize(
+              N = length(log(!!sym(input$vitalvar))), 
+              Mean = mean(log(!!sym(input$vitalvar)), na.rm = T),
+              Std = sd(log(!!sym(input$vitalvar)), na.rm = T),
+              Min = min(log(!!sym(input$vitalvar)), na.rm = T),
+              Median = median(log(!!sym(input$vitalvar)), na.rm = T),
+              Max = max(log(!!sym(input$vitalvar)), na.rm = T),
+              `N missing` = sum(is.na(!!sym(input$vitalvar))
+              ))
+        } else {
+          icu_cohort %>%
+            summarize(
+              N = length(!!sym(input$vitalvar)),
+              Mean = mean(!!sym(input$vitalvar), na.rm = T),
+              Std = sd(!!sym(input$vitalvar), na.rm = T),
+              Min = min(!!sym(input$vitalvar), na.rm = T),
+              Median = median(!!sym(input$vitalvar), na.rm = T),
+              Max = max(!!sym(input$vitalvar), na.rm = T),
+              `N missing` = sum(is.na(!!sym(input$vitalvar))
+              ))
+        }
+        
+      }
+    )
   
-  output$labtable <- renderTable(
-    icu_cohort %>%
-      summarize(
-        N = length(!!sym(input$labvar)), 
-        Mean = mean(!!sym(input$labvar), na.rm = T),
-        Std = sd(!!sym(input$labvar), na.rm = T),
-        Min = min(!!sym(input$labvar), na.rm = T),
-        Median = median(!!sym(input$labvar), na.rm = T),
-        Max = max(!!sym(input$labvar), na.rm = T),
-        `N missing` = sum(is.na(!!sym(input$labvar))
-        ))
-  )
+    #---- **Histogram/bar chart ----
+    output$vitalplot <- renderPlot(
+      {if(input$vitalvar %in% catvar){
+        icu_cohort %>%
+          ggplot(., aes(x = as.character(!!sym(input$vitalvar)))) + 
+          geom_bar(aes(x = as.character(!!sym(input$vitalvar)))) +
+          theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)) + 
+          labs(title = paste0("Bar chart of ", input$vitalvar))
+      }
+        else{
+        if (input$vitallog){
+          icu_cohort %>%
+            ggplot(., aes(x = log(!!sym(input$vitalvar)))) +
+            geom_histogram(bins = input$binsvital, 
+                           colour = "black", fill = "grey") +
+            #Pending vitalel
+            # xlab() +
+            ylab("Density")
+          
+        } else{
+          icu_cohort %>%
+            ggplot(., aes_string(x = input$vitalvar)) +
+            geom_histogram(bins = input$binsvital, 
+                           colour = "black", fill = "grey") +
+            #Pending label
+            # xlab() +
+            ylab("Density")
+        }
+        }
+      }, 
+      height = 400, width = 600
+    ) 
+    #---- **Boxplot ----
+    output$vitalboxplot <- renderPlot(
+      {
+        if (input$vitallog){
+          icu_cohort %>%
+            ggplot(., aes(x = log(!!sym(input$vitalvar)))) +
+            geom_boxplot(color = "black", fill = "grey") +
+            xlab(input$vitalvar)
+        } else{
+          icu_cohort %>%
+            ggplot(., aes_string(x = input$vitalvar)) +
+            geom_boxplot(color = "black", fill = "grey") +
+            xlab(input$vitalvar)
+        }
+      }, 
+      height = 400, width = 600
+    )
   
-  output$labplot <- renderPlot(
-    icu_cohort %>%
-      ggplot(., aes_string(x = input$labvar)) +
-      geom_histogram(bins = input$binslab, 
-                     colour = "grey", fill = "light blue") +
-      #Pending label
-      # xlab() +
-      ylab("Density")
-  )
-
-  output$vitaltable <- renderTable(
-    icu_cohort %>%
-      summarize(
-        N = length(!!sym(input$vitalvar)),
-        Mean = mean(!!sym(input$vitalvar), na.rm = T),
-        Std = sd(!!sym(input$vitalvar), na.rm = T),
-        Min = min(!!sym(input$vitalvar), na.rm = T),
-        Median = median(!!sym(input$vitalvar), na.rm = T),
-        Max = max(!!sym(input$vitalvar), na.rm = T),
-        `N missing` = sum(is.na(!!sym(input$vitalvar))
-        ))
-  )
-  
-  output$vitalplot <- renderPlot(
-    icu_cohort %>%
-      ggplot(., aes_string(x = log(input$vitalvar))) +
-      geom_histogram(bins = input$binsvital, 
-                     colour = "grey", fill = "light blue") +
-      #Pending label
-      # xlab() +
-      ylab("Density")
-  )
-  
- #  output$distPlotdemo <- renderPlot({
- # 
- #  })
- #  output$distTable <- {
- #    icu_cohort %>%
- #      kbl(caption = "Direct and indirect effects of X on Y through M")%>%
- #      kable_classic(full_width = F, html_font = "Arial")
- #  }
- }
+}
 
 
 #---- Run the app ----
